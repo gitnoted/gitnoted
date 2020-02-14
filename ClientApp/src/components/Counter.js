ff734f4da4c1f5a76e8b0173bf7e5ca71425fbfb
laplaceliu <laplaceliu@outlook.com>
2020-02-15 00:28:08 +0800

    test tailwindcss

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -24,7 +24,10 @@ export class Counter extends Component {
 
         <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={this.incrementCounter}>
+            Increment
+        </button>
       </div>
     );
   }
